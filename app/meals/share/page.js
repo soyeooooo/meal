eaee1607c3ac358aeee104e47343c9017d578756
soyeooooo <alththdus1@naver.com>
2025-01-13 01:52:01 +0900

    feat : upload img
    이미지 업로드하기

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,22 +1,13 @@
+'use client'
+
 import IMAGEPicker from '../image_picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/ownmeals';
 
 
 export default function ShareMealPage() {
-  async function shareMeal(formData) {
-    'use server';
 
-    const meal = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email')
-    };
-    console.log(meal);
-  }
-  
+
   return (
     <>
       <header className={classes.header}>
